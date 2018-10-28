aae4c4474008c52df9afec3b010cd0ecb166579d
MJaroslav <rcr3211@gmail.com>
2018-10-29 02:09:42 +0300

    Help fixes

--- src/main/java/mjaroslav/bots/core/amadeus/commands/CommandHelp.java
+++ src/main/java/mjaroslav/bots/core/amadeus/commands/CommandHelp.java
@@ -17,17 +17,28 @@ public class CommandHelp extends BaseCommand {
     @Override
     public void execute(IUser sender, IMessage source, String args) throws Exception {
         if (args.length() > 0) {
+            EmbedBuilder builder = new EmbedBuilder().withColor(0x00FF00);
+            StringBuilder desc = new StringBuilder();
             HashMap<String, String> argsParsed = AmadeusUtils.parseArgsToMap(args);
             CommandHandler handler = null;
-            if (hasArg(source, "handler", argsParsed))
-                handler = core.getCommandHandler(argValue(source, "handler", argsParsed));
+            if (hasArg(source, "handler", argsParsed)) {
+                if (argValue(source, "handler", argsParsed).equals("all")) {
+                    builder.withAuthorName(core.langs.translate(source, "help_name", "all"));
+                    desc.append(core.langs.translate(source, "help_command_default_help_handler") + "\n\n");
+                    desc.append(core.langs.translate(source, "help_handlers") + "\n");
+                    for (CommandHandler han : core.listOfCommandHandlers())
+                        desc.append("`" + han.name + "` ");
+                    builder.withDesc(desc.toString().trim());
+                    core.answerMessage(source, builder.build());
+                    return;
+                } else
+                    handler = core.getCommandHandler(argValue(source, "handler", argsParsed));
+            }
             if (handler == null)
                 handler = this.handler;
             BaseCommand command = null;
             if (hasArg(source, "command", argsParsed))
                 command = handler.getCommand(source.getGuild(), sender, argValue(source, "command", argsParsed));
-            EmbedBuilder builder = new EmbedBuilder().withColor(0x00FF00);
-            StringBuilder desc = new StringBuilder();
             if (command != null) {
                 builder.withAuthorName(core.langs.translate(source.getGuild(), sender, "help_name",
                         handler.name + " > " + command.name));
@@ -36,29 +47,30 @@ public class CommandHelp extends BaseCommand {
                     argName = argValue(source, "arg", argsParsed);
                 if (AmadeusUtils.stringIsEmpty(argName)) {
                     desc.append(command.getHelpDesc(source));
+                    desc.append("\n\n" + core.langs.translate(source, "help_command_default_help_arg"));
                     desc.append("\n\n" + core.langs.translate(source, "help_synonyms") + "\n");
                     for (String name : core.langs.getNames(source, command))
-                        desc.append("\"" + name + "\" ");
+                        desc.append("`" + name + "` ");
                     if (!command.getArgsList().isEmpty())
                         desc.append("\n\n" + core.langs.translate(source, "help_args") + "\n");
                     for (String arg : command.getArgsList())
-                        desc.append("\"" + arg + "\" ");
+                        desc.append("`" + arg + "` ");
                 } else {
                     desc.append(command.getHelpDesc(source, argName));
                     desc.append("\n\n" + core.langs.translate(source, "help_synonyms") + "\n");
                     for (String name : core.langs.getNamesArg(source, command, argName))
-                        desc.append("\"" + name + "\" ");
+                        desc.append("`" + name + "` ");
                     builder.withAuthorName(core.langs.translate(source, "help_name",
                             handler.name + " > " + command.name + " > " + argName));
                 }
-
                 builder.withDesc(desc.toString().trim());
                 core.answerMessage(source, builder.build());
             } else {
                 builder.withAuthorName(core.langs.translate(source, "help_name", handler.name));
+                desc.append(core.langs.translate(source, "help_command_default_help_command") + "\n\n");
                 desc.append(core.langs.translate(source, "help_commands") + "\n");
                 for (BaseCommand com : handler.getCommandList())
-                    desc.append("\"" + com.name + "\" ");
+                    desc.append("`" + com.name + "` ");
                 builder.withDesc(desc.toString().trim());
                 core.answerMessage(source, builder.build());
             }
